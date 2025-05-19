757c060770191d63768ed7f7d8d02c756ff4273e
Gal Ben Haim <gbenhaim@redhat.com>
2025-05-20 01:02:04 +0300

    add failure
    Signed-off-by: Gal Ben Haim <gbenhaim@redhat.com>

--- promote.py
+++ promote.py
@@ -3,10 +3,18 @@
 from shutil import copytree
 from itertools import pairwise
 from pathlib import Path
+from sys import argv
 
 def main() -> None:
+    fail_in_ring = ""
+    if len(argv) > 1:
+        fail_in_ring = argv[1]
+
     rings = list(Path("rings").iterdir())
     for current, prev in pairwise(reversed(rings)):
+        if fail_in_ring == prev.name:
+            print(f"!!! Failed to promote from {prev} to {current}")
+            continue
         copytree(prev, current, dirs_exist_ok=True)
         print(f"Promoted {prev} to {current}")
 
